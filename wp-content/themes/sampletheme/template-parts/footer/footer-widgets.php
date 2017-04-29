849fd304035462d7ffb4655ebca8090c500a9909
echizenyayota <virtualmisso30@gmail.com>
2017-04-29 12:15:42 +0900

    P202 TRY1

--- wp-content/themes/sampletheme/template-parts/footer/footer-widgets.php
+++ wp-content/themes/sampletheme/template-parts/footer/footer-widgets.php
@@ -1,13 +1,15 @@
-<aside class="widget-area" role="complementary">
-	<?php
-	if ( is_active_sidebar( 'footer-1' ) ) { ?>
-		<div class="widget-column footer-widget-1">
-			<?php dynamic_sidebar( 'footer-1' ); ?>
-		</div>
-	<?php }
-	if ( is_active_sidebar( 'footer-2' ) ) { ?>
-		<div class="widget-column footer-widget-2">
-			<?php dynamic_sidebar( 'footer-2' ); ?>
-		</div>
-	<?php } ?>
-</aside><!-- .widget-area -->
+<?php if ( is_active_sidebar( 'footer_1' ) || is_active_sidebar ( 'footer_2' )) :?>
+	<aside class="widget-area" role="complementary">
+		<?php
+		if ( is_active_sidebar( 'footer-1' ) ) { ?>
+			<div class="widget-column footer-widget-1">
+				<?php dynamic_sidebar( 'footer-1' ); ?>
+			</div>
+		<?php }
+		if ( is_active_sidebar( 'footer-2' ) ) { ?>
+			<div class="widget-column footer-widget-2">
+				<?php dynamic_sidebar( 'footer-2' ); ?>
+			</div>
+		<?php } ?>
+	</aside><!-- .widget-area -->
+<?php endif; ?>
